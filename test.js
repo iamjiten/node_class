515dfd60d5af2458d73bc255e7b180d236627793
JITEN <jitentharu143@gmail.com>
2025-08-15 15:01:16 +0545

    typescript fundamental

---
+++ test.js
@@ -0,0 +1,18 @@
+// let num1 = 10;
+// num1 = 20;
+// let num1 = 10;
+// let num2 = 20;
+// const result = num1 + num2;
+// console.log({ result });
+// function sayHello() {
+//   console.log("Hello world");
+// }
+// sayHello();
+function sayHello(name, age) {
+    console.log("Hello " + name + ". You are " + age + " years old");
+}
+sayHello("jiten", 10);
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add(10, 15));
